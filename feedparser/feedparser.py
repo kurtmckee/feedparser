98be1f861304f07c03045e421e3f11ef1b0d7004
f8dy <f8dy@73d2b349-402e-0410-baf4-070fd12ab5b7>
2004-07-12 12:58:56 +0000

    988356: support for Greek dates
    git-svn-id: http://feedparser.googlecode.com/svn/trunk@86 73d2b349-402e-0410-baf4-070fd12ab5b7

--- feedparser/feedparser.py
+++ feedparser/feedparser.py
@@ -1701,19 +1701,19 @@ _korean_onblog_date_re = \
     re.compile('(\d{4})%s\s+(\d{2})%s\s+(\d{2})%s\s+(\d{2}):(\d{2}):(\d{2})' % \
                (_korean_year, _korean_month, _korean_day))
 _korean_nate_date_re = \
-  re.compile(u'(\d{4})-(\d{2})-(\d{2})\s+(%s|%s)\s+(\d{,2}):(\d{,2}):(\d{,2})' % \
-             (_korean_am, _korean_pm))
+    re.compile(u'(\d{4})-(\d{2})-(\d{2})\s+(%s|%s)\s+(\d{,2}):(\d{,2}):(\d{,2})' % \
+               (_korean_am, _korean_pm))
 _mssql_date_re = \
-  re.compile('(\d{4})-(\d{2})-(\d{2})\s+(\d{2}):(\d{2}):(\d{2})\.\d+')
+    re.compile('(\d{4})-(\d{2})-(\d{2})\s+(\d{2}):(\d{2}):(\d{2})\.\d+')
 
 def _parse_date_onblog(dateString):
     """Parse a string according to the OnBlog 8-bit date format"""
     m = _korean_onblog_date_re.match(dateString)
     if not m: return
     w3dtfdate = "%(year)s-%(month)s-%(day)sT%(hour)s:%(minute)s:%(second)s%(zonediff)s" % \
-           {'year': m.group(1), 'month': m.group(2), 'day': m.group(3),\
-            'hour': m.group(4), 'minute': m.group(5), 'second': m.group(6),\
-            'zonediff': '+09:00'}
+                {'year': m.group(1), 'month': m.group(2), 'day': m.group(3),\
+                 'hour': m.group(4), 'minute': m.group(5), 'second': m.group(6),\
+                 'zonediff': '+09:00'}
     if _debug: sys.stderr.write("OnBlog date parsed as: %s\n" % w3dtfdate)
     return _parse_date_w3dtf(w3dtfdate)
 registerDateHandler(_parse_date_onblog)
@@ -1730,9 +1730,9 @@ def _parse_date_nate(dateString):
     if len(hour) == 1:
         hour = '0' + hour
     w3dtfdate = "%(year)s-%(month)s-%(day)sT%(hour)s:%(minute)s:%(second)s%(zonediff)s" % \
-           {'year': m.group(1), 'month': m.group(2), 'day': m.group(3),\
-            'hour': hour, 'minute': m.group(6), 'second': m.group(7),\
-            'zonediff': '+09:00'}
+                {'year': m.group(1), 'month': m.group(2), 'day': m.group(3),\
+                 'hour': hour, 'minute': m.group(6), 'second': m.group(7),\
+                 'zonediff': '+09:00'}
     if _debug: sys.stderr.write("Nate date parsed as: %s\n" % w3dtfdate)
     return _parse_date_w3dtf(w3dtfdate)
 registerDateHandler(_parse_date_nate)
@@ -1742,12 +1742,67 @@ def _parse_date_mssql(dateString):
     m = _mssql_date_re.match(dateString)
     if not m: return
     w3dtfdate = "%(year)s-%(month)s-%(day)sT%(hour)s:%(minute)s:%(second)s%(zonediff)s" % \
-           {'year': m.group(1), 'month': m.group(2), 'day': m.group(3),\
-            'hour': m.group(4), 'minute': m.group(5), 'second': m.group(6),\
-            'zonediff': '+09:00'}
+                {'year': m.group(1), 'month': m.group(2), 'day': m.group(3),\
+                 'hour': m.group(4), 'minute': m.group(5), 'second': m.group(6),\
+                 'zonediff': '+09:00'}
     if _debug: sys.stderr.write("MS SQL date parsed as: %s\n" % w3dtfdate)
     return _parse_date_w3dtf(w3dtfdate)
 registerDateHandler(_parse_date_mssql)
+
+# Unicode strings for Greek date strings
+_greek_months = \
+  { \
+   u'\u0399\u03b1\u03bd': u'Jan',       # c9e1ed in iso-8859-7
+   u'\u03a6\u03b5\u03b2': u'Feb',       # d6e5e2 in iso-8859-7
+   u'\u039c\u03ac\u03ce': u'Mar',       # ccdcfe in iso-8859-7
+   u'\u039c\u03b1\u03ce': u'Mar',       # cce1fe in iso-8859-7
+   u'\u0391\u03c0\u03c1': u'Apr',       # c1f0f1 in iso-8859-7
+   u'\u039c\u03ac\u03b9': u'May',       # ccdce9 in iso-8859-7
+   u'\u039c\u03b1\u03ca': u'May',       # cce1fa in iso-8859-7
+   u'\u039c\u03b1\u03b9': u'May',       # cce1e9 in iso-8859-7
+   u'\u0399\u03bf\u03cd\u03bd': u'Jun', # c9effded in iso-8859-7
+   u'\u0399\u03bf\u03bd': u'Jun',       # c9efed in iso-8859-7
+   u'\u0399\u03bf\u03cd\u03bb': u'Jul', # c9effdeb in iso-8859-7
+   u'\u0399\u03bf\u03bb': u'Jul',       # c9f9eb in iso-8859-7
+   u'\u0391\u03cd\u03b3': u'Aug',       # c1fde3 in iso-8859-7
+   u'\u0391\u03c5\u03b3': u'Aug',       # c1f5e3 in iso-8859-7
+   u'\u03a3\u03b5\u03c0': u'Sep',       # d3e5f0 in iso-8859-7
+   u'\u039f\u03ba\u03c4': u'Oct',       # cfeaf4 in iso-8859-7
+   u'\u039d\u03bf\u03ad': u'Nov',       # cdefdd in iso-8859-7
+   u'\u039d\u03bf\u03b5': u'Nov',       # cdefe5 in iso-8859-7
+   u'\u0394\u03b5\u03ba': u'Dec',       # c4e5ea in iso-8859-7
+  }
+
+_greek_wdays = \
+  { \
+   u'\u039a\u03c5\u03c1': u'Sun', # caf5f1 in iso-8859-7
+   u'\u0394\u03b5\u03c5': u'Mon', # c4e5f5 in iso-8859-7
+   u'\u03a4\u03c1\u03b9': u'Tue', # d4f1e9 in iso-8859-7
+   u'\u03a4\u03b5\u03c4': u'Wed', # d4e5f4 in iso-8859-7
+   u'\u03a0\u03b5\u03bc': u'Thu', # d0e5ec in iso-8859-7
+   u'\u03a0\u03b1\u03c1': u'Fri', # d0e1f1 in iso-8859-7
+   u'\u03a3\u03b1\u03b2': u'Sat', # d3e1e2 in iso-8859-7   
+  }
+
+_greek_date_format_re = \
+    re.compile(u'([^,]+),\s+(\d{2})\s+([^\s]+)\s+(\d{4})\s+(\d{2}):(\d{2}):(\d{2})\s+([^\s]+)')
+
+def _parse_date_greek(dateString):
+    """Parse a string according to a Greek 8-bit date format."""
+    m = _greek_date_format_re.match(dateString)
+    if not m: return
+    try:
+        wday = _greek_wdays[m.group(1)]
+        month = _greek_months[m.group(3)]
+    except:
+        return
+    rfc822date = "%(wday)s, %(day)s %(month)s %(year)s %(hour)s:%(minute)s:%(second)s %(zonediff)s" % \
+                 {'wday': wday, 'day': m.group(2), 'month': month, 'year': m.group(4),\
+                  'hour': m.group(5), 'minute': m.group(6), 'second': m.group(7),\
+                  'zonediff': m.group(8)}
+    if _debug: sys.stderr.write("Greek date parsed as: %s\n" % rfc822date)
+    return _parse_date_rfc822(rfc822date)
+registerDateHandler(_parse_date_greek)
 
 # W3DTF-style date parsing adapted from PyXML xml.utils.iso8601, written by
 # Drake and licensed under the Python license.  Removed all range checking
@@ -2460,13 +2515,13 @@ if __name__ == '__main__':
 #  doesn't, first grandchild redeclares it, and second grandchild doesn't;
 #  refactored date parsing; defined public registerDateHandler so callers
 #  can add support for additional date formats at runtime; added support
-#  for OnBlog, Nate, and MSSQL dates (ytrewq1); zopeCompatibilityHack()
-#  turns FeedParserDict into a regular dictionary, required for Zope
-#  compatibility, and also makes command-line debugging easier because
-#  pprint module formats real dictionaries better than dictionary-like
-#  objects; added NonXMLContentType exception, which is stored in
-#  bozo_exception when a feed is served with a non-XML media type such
-#  as "text/plain"; ad
+#  for OnBlog, Nate, MSSQL, and Greek dates (ytrewq1); added
+#  zopeCompatibilityHack() which turns FeedParserDict into a regular
+#  dictionary, required for Zope compatibility, and also makes command-
+#  line debugging easier because pprint module formats real dictionaries
+#  better than dictionary-like objects; added NonXMLContentType exception,
+#  which is stored in bozo_exception when a feed is served with a non-XML
+#  media type such as "text/plain"
 
 # TODO
 # - add Content-Language + test cases
