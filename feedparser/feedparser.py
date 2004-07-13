7f1330fabdd4c2c7ef44b7abf81a25f070596e58
f8dy <f8dy@73d2b349-402e-0410-baf4-070fd12ab5b7>
2004-07-13 15:00:06 +0000

    989890: support for Hungarian dates
    git-svn-id: http://feedparser.googlecode.com/svn/trunk@89 73d2b349-402e-0410-baf4-070fd12ab5b7

--- feedparser/feedparser.py
+++ feedparser/feedparser.py
@@ -1806,6 +1806,48 @@ def _parse_date_greek(dateString):
     return _parse_date_rfc822(rfc822date)
 registerDateHandler(_parse_date_greek)
 
+# Unicode strings for Hungarian date strings
+_hungarian_months = \
+  { \
+    u'janu\u00e1r':   u'01',  # e1 in iso-8859-2
+    u'febru\u00e1ri': u'02',  # e1 in iso-8859-2
+    u'm\u00e1rcius':  u'03',  # e1 in iso-8859-2
+    u'\u00e1prilis':  u'04',  # e1 in iso-8859-2
+    u'm\u00e1ujus':   u'05',  # e1 in iso-8859-2
+    u'j\u00fanius':   u'06',  # fa in iso-8859-2
+    u'j\u00falius':   u'07',  # fa in iso-8859-2
+    u'augusztus':     u'08',
+    u'szeptember':    u'09',
+    u'okt\u00f3ber':  u'10',  # f3 in iso-8859-2
+    u'november':      u'11',
+    u'december':      u'12',
+  }
+
+_hungarian_date_format_re = \
+  re.compile(u'(\d{4})-([^-]+)-(\d{,2})T(\d{,2}):(\d{2})((\+|-)(\d{,2}:\d{2}))')
+
+def _parse_date_hungarian(dateString):
+    """Parse a string according to a Hungarian 8-bit date format."""
+    m = _hungarian_date_format_re.match(dateString)
+    if not m: return
+    try:
+        month = _hungarian_months[m.group(2)]
+        day = m.group(3)
+        if len(day) == 1:
+            day = '0' + day
+        hour = m.group(4)
+        if len(hour) == 1:
+            hour = '0' + hour
+    except:
+        return
+    w3dtfdate = "%(year)s-%(month)s-%(day)sT%(hour)s:%(minute)s%(zonediff)s" % \
+                {'year': m.group(1), 'month': month, 'day': day,\
+                 'hour': hour, 'minute': m.group(5),\
+                 'zonediff': m.group(6)}
+    if _debug: sys.stderr.write("Hungarian date parsed as: %s\n" % w3dtfdate)
+    return _parse_date_w3dtf(w3dtfdate)
+registerDateHandler(_parse_date_hungarian)
+
 # W3DTF-style date parsing adapted from PyXML xml.utils.iso8601, written by
 # Drake and licensed under the Python license.  Removed all range checking
 # for month, day, hour, minute, and second, since mktime will normalize
@@ -2496,7 +2538,7 @@ if __name__ == '__main__':
 #  doesn't, first grandchild redeclares it, and second grandchild doesn't;
 #  refactored date parsing; defined public registerDateHandler so callers
 #  can add support for additional date formats at runtime; added support
-#  for OnBlog, Nate, MSSQL, and Greek dates (ytrewq1); added
+#  for OnBlog, Nate, MSSQL, Greek, and Hungarian dates (ytrewq1); added
 #  zopeCompatibilityHack() which turns FeedParserDict into a regular
 #  dictionary, required for Zope compatibility, and also makes command-
 #  line debugging easier because pprint module formats real dictionaries
