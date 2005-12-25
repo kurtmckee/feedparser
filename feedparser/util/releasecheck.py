f2be8e36f9bf31c77fe1adadd95ac3565d2d75bc
f8dy <f8dy@73d2b349-402e-0410-baf4-070fd12ab5b7>
2005-12-25 03:22:56 +0000

    *** empty log message ***
    git-svn-id: http://feedparser.googlecode.com/svn/trunk@174 73d2b349-402e-0410-baf4-070fd12ab5b7

---
+++ feedparser/util/releasecheck.py
@@ -0,0 +1,72 @@
+import feedparser
+import feedparsertest
+import sys, re, time
+
+if feedparser._debug:
+    sys.stderr.write('feedparser._debug == 1\n')
+    sys.exit(1)
+
+if feedparsertest._debug:
+    sys.stderr.write('feedparsertest._debug == 1\n');
+    sys.exit(1)
+
+makefile_version = None
+feedparser_version = None
+docs_version = None
+docs_date = None
+
+makefile_re = re.compile(r'^VERSION\s*=\s*(.*?)$')
+for line in file('Makefile'):
+    m = makefile_re.search(line)
+    if m:
+        makefile_version = m.group(1)
+        break
+if not makefile_version:
+    sys.stderr.write('could not determine Makefile version\n')
+    sys.exit(1)
+
+feedparser_re = re.compile(r'^__version__\s*=\s*["\'](.*?)["\']')
+for line in file('feedparser.py'):
+    m = feedparser_re.search(line)
+    if m:
+        feedparser_version = m.group(1)
+        break
+if not feedparser_version:
+    sys.stderr.write('could not determine feedparser.py version\n')
+    sys.exit(1)
+
+docs_re = re.compile(r'^<!ENTITY\s*feedparser_version\s*["\'](.*?)["\']>$')
+for line in file('docs/xml/feedparser.xml'):
+    m = docs_re.search(line)
+    if m:
+        docs_version = m.group(1)
+        break
+if not docs_version:
+    sys.stderr.write('could not determine feedparser docs version\n')
+    sys.exit(1)
+
+if makefile_version != feedparser_version:
+    sys.stderr.write('Makefile version = %s, but feedparser.py version = %s\n' % (makefile_version, feedparser_version))
+    sys.exit(1)
+
+if makefile_version != docs_version:
+    sys.stderr.write('Makefile version = %s, but docs version = %s\n' % (makefile_version, docs_version))
+    sys.exit(1)
+
+if feedparser_version != docs_version:
+    sys.stderr.write('feedparser.py version = %s, but docs version = %s\n' % (feedparser_version, docs_version))
+    sys.exit(1)
+
+date_re = re.compile(r'^<!ENTITY\s*fileversion\s*["\'](.*?)["\']>$')
+for line in file('docs/xml/version.xml'):
+    m = date_re.search(line)
+    if m:
+        docs_date = m.group(1)
+if not docs_date:
+    sys.stderr.write('could not determine feedparser docs date\n')
+    sys.exit(1)
+
+todays_date = time.strftime('%Y-%m-%d')
+if docs_date != todays_date:
+    sys.stderr.write('docs date = %s but today is %s\n' % (docs_date, todays_date))
+    sys.exit(1)
