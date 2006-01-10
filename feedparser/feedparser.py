425f6890883351c46be6c33cf2fe4b081782d97c
f8dy <f8dy@73d2b349-402e-0410-baf4-070fd12ab5b7>
2006-01-10 18:31:41 +0000

    bug [ 1399464 ] Bad regexp for _shorttag_replace
    git-svn-id: http://feedparser.googlecode.com/svn/trunk@179 73d2b349-402e-0410-baf4-070fd12ab5b7

--- feedparser/feedparser.py
+++ feedparser/feedparser.py
@@ -1432,7 +1432,8 @@ class _BaseHTMLProcessor(sgmllib.SGMLParser):
         
     def feed(self, data):
         data = re.compile(r'<!((?!DOCTYPE|--|\[))', re.IGNORECASE).sub(r'&lt;!\1', data)
-        data = re.sub(r'<(\S+?)\s*?/>', self._shorttag_replace, data)
+        #data = re.sub(r'<(\S+?)\s*?/>', self._shorttag_replace, data) # bug [ 1399464 ] Bad regexp for _shorttag_replace
+        data = re.sub(r'<([^<\s]+?)\s*/>', self._shorttag_replace, data) 
         data = data.replace('&#39;', "'")
         data = data.replace('&#34;', '"')
         if self.encoding and type(data) == type(u''):
