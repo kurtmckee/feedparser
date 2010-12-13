2b3ca581baa272e086eafc92dd14a2562853e2cc
adewale <adewale@73d2b349-402e-0410-baf4-070fd12ab5b7>
2010-12-13 01:51:10 +0000

    Applied patch for issue 39
    git-svn-id: http://feedparser.googlecode.com/svn/trunk@338 73d2b349-402e-0410-baf4-070fd12ab5b7

--- feedparser/feedparser.py
+++ feedparser/feedparser.py
@@ -172,7 +172,7 @@ class UndeclaredNamespace(Exception): pass
 
 sgmllib.tagfind = re.compile('[a-zA-Z][-_.:a-zA-Z0-9]*')
 sgmllib.special = re.compile('<!')
-sgmllib.charref = re.compile('&#(\d+|x[0-9a-fA-F]+);')
+sgmllib.charref = re.compile('&#(\d+|[xX][0-9a-fA-F]+);')
 
 if sgmllib.endbracket.search(' <').start(0):
     class EndBracketMatch:
