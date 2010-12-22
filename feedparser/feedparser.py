64b73b406bff9bf9c429a8e76ff0c15c8eca5a40
adewale <adewale@73d2b349-402e-0410-baf4-070fd12ab5b7>
2010-12-22 21:20:38 +0000

    Applied patch for issue 118 - added support for feed: pseudo-protocol.
    git-svn-id: http://feedparser.googlecode.com/svn/trunk@343 73d2b349-402e-0410-baf4-070fd12ab5b7

--- feedparser/feedparser.py
+++ feedparser/feedparser.py
@@ -2730,7 +2730,12 @@ def _open_resource(url_file_stream_or_string, etag, modified, agent, referrer, h
     if url_file_stream_or_string == '-':
         return sys.stdin
 
-    if urlparse.urlparse(url_file_stream_or_string)[0] in ('http', 'https', 'ftp', 'file'):
+    if urlparse.urlparse(url_file_stream_or_string)[0] in ('http', 'https', 'ftp', 'file', 'feed'):
+        # Deal with the feed URI scheme
+        if url_file_stream_or_string.startswith('feed:http'):
+            url_file_stream_or_string = url_file_stream_or_string[5:]
+        elif url_file_stream_or_string.startswith('feed:'):
+            url_file_stream_or_string = 'http:' + url_file_stream_or_string[5:]
         if not agent:
             agent = USER_AGENT
         # test for inline user:password for basic auth
