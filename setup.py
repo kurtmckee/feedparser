b4ad607c6ea75a2026626bff3eb0ecd6bbbbe3d2
kurtmckee <kurtmckee@73d2b349-402e-0410-baf4-070fd12ab5b7>
2011-01-20 14:11:43 +0000

    Clean up and modernize setup.py
    * Removed ancient Python hacks
    * Reuse version and long description in feedparser.py
    * Updated and sorted keywords
    * Attempted to clean up classifiers
    * Standardized whitespacing and quotes
    * Accounted for new location of setup.py
    
    git-svn-id: http://feedparser.googlecode.com/svn/trunk@365 73d2b349-402e-0410-baf4-070fd12ab5b7

--- setup.py
+++ setup.py
@@ -1,47 +1,39 @@
 from distutils.core import setup
+import imp
+import os.path
 
-# patch distutils if it can't cope with the "classifiers" or "download_url"
-# keywords (prior to python 2.3.0).
-from distutils.dist import DistributionMetadata
-if not hasattr(DistributionMetadata, 'classifiers'):
-    DistributionMetadata.classifiers = None
-if not hasattr(DistributionMetadata, 'download_url'):
-    DistributionMetadata.download_url = None
-    
+path = os.path.join('feedparser', 'feedparser.py')
+feedparser = imp.load_source('feedparser', path)
+
 setup(
     name = 'feedparser',
-    version = '4.1',
+    version = feedparser.__version__,
     description = 'Universal feed parser, handles RSS 0.9x, RSS 1.0, '
                   'RSS 2.0, CDF, Atom 0.3, and Atom 1.0 feeds',
-    long_description = """\
-Universal feed parser
----------------------
-
-Handles RSS 0.9x, RSS 1.0, RSS 2.0, CDF, Atom 0.3, Atom 1.0 feeds
-
-Required: Python 2.1 or later
-Recommended: Python 2.3 or later
-Recommended: libxml2 <http://xmlsoft.org/python.html>
-""",
-    author='Mark Pilgrim',
+    long_description = feedparser.__doc__.strip(),
+    author = 'Mark Pilgrim',
     author_email = 'mark@diveintomark.org',
     url = 'http://feedparser.org/',
     download_url = 'http://code.google.com/p/feedparser/',
-    license = "MIT",
     platforms = ['POSIX', 'Windows'],
-    keywords = ['feed parser', 'feeds', 'rss', 'atom', 'cdf'],
+    package_dir = {'': 'feedparser'},
+    py_modules = ['feedparser'],
+    keywords = ['atom', 'cdf', 'feed', 'parser', 'rdf', 'rss'],
     classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Other Environment",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Communications",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Other/Nonlisted Topic",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-    py_modules = ['feedparser',]
-    )
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: XML',
+    ]
+)
