c02c355cb347cf5296a891303e0af8b6b9a321e1
kurtmckee <kurtmckee@73d2b349-402e-0410-baf4-070fd12ab5b7>
2011-04-26 17:16:35 +0000

    Remove `UserDict` and `dict` compatibility code
    git-svn-id: http://feedparser.googlecode.com/svn/trunk@427 73d2b349-402e-0410-baf4-070fd12ab5b7

--- feedparser/feedparsertest.py
+++ feedparser/feedparsertest.py
@@ -29,14 +29,8 @@ import feedparser, unittest, os, sys, glob, re, urllib, posixpath, time, codecs,
 if not feedparser._XML_AVAILABLE:
   sys.stderr.write('No XML parsers available, unit testing can not proceed\n')
   sys.exit(1)
-# This line appears to be unnecessary
-#from UserDict import UserDict
 import SimpleHTTPServer, BaseHTTPServer
 from threading import *
-try:
-  dict
-except NameError:
-  from feedparser import dict
 
 _debug = 0
 try:
