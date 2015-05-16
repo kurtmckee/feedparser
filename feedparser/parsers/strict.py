b43a4f10fb2f0f80ec7b0ac3aa469d1506983c65
Kurt McKee <contactme@kurtmckee.org>
2015-05-15 20:12:21 -0500

    Clean up the XML handler functions
    * Reduce the times of times lower() is called
    * Remove all of the unused qname-based prefix normalization code
    * Remove the unnecessary string search for 'backend.userland.com'
    * Rearrange the functions a little bit
    * Switch from string concatenation to str.format()

--- feedparser/parsers/strict.py
+++ feedparser/parsers/strict.py
@@ -28,8 +28,6 @@
 
 from __future__ import absolute_import, unicode_literals
 
-from ..exceptions import UndeclaredNamespace
-
 class _StrictFeedParser(object):
     def __init__(self, baseuri, baselang, encoding):
         self.bozo = 0
@@ -41,7 +39,7 @@ class _StrictFeedParser(object):
         super(_StrictFeedParser, self).__init__()
 
     def _normalize_attributes(self, kv):
-        k = kv[0].lower()
+        k = kv[0]
         v = k in ('rel', 'type') and kv[1].lower() or kv[1]
         return (k, v)
 
@@ -53,73 +51,52 @@ class _StrictFeedParser(object):
             self.decls['xmlns:' + prefix] = uri
 
     def startElementNS(self, name, qname, attrs):
-        namespace, localname = name
-        lowernamespace = str(namespace or '').lower()
-        if lowernamespace.find('backend.userland.com/rss') != -1:
-            # match any backend.userland.com namespace
-            namespace = 'http://backend.userland.com/rss'
-            lowernamespace = namespace
-        if qname and qname.find(':') > 0:
-            givenprefix = qname.split(':')[0]
-        else:
-            givenprefix = None
-        prefix = self._matchnamespaces.get(lowernamespace, givenprefix)
-        if givenprefix and (prefix == None or (prefix == '' and lowernamespace == '')) and givenprefix not in self.namespacesInUse:
-            raise UndeclaredNamespace("'%s' is not associated with a namespace" % givenprefix)
-        localname = str(localname).lower()
+        uri, localname = name
+        localname = localname.lower()
+        loweruri = (uri or '').lower()
+        prefix = self._matchnamespaces.get(loweruri)
 
-        # qname implementation is horribly broken in Python 2.1 (it
-        # doesn't report any), and slightly broken in Python 2.2 (it
-        # doesn't report the xml: namespace). So we match up namespaces
-        # with a known list first, and then possibly override them with
-        # the qnames the SAX parser gives us (if indeed it gives us any
-        # at all).  Thanks to MatejC for helping me test this and
-        # tirelessly telling me that it didn't work yet.
         attrsD, self.decls = self.decls, {}
-        if localname=='math' and namespace=='http://www.w3.org/1998/Math/MathML':
-            attrsD['xmlns']=namespace
-        if localname=='svg' and namespace=='http://www.w3.org/2000/svg':
-            attrsD['xmlns']=namespace
+        if localname == 'math' and uri == 'http://www.w3.org/1998/Math/MathML':
+            attrsD['xmlns'] = uri
+        if localname == 'svg' and uri == 'http://www.w3.org/2000/svg':
+            attrsD['xmlns'] = uri
 
-        if prefix:
-            localname = prefix.lower() + ':' + localname
-        elif namespace and not qname: #Expat
-            for name,value in self.namespacesInUse.items():
-                if name and value == namespace:
-                    localname = name + ':' + localname
-                    break
+        for key, value in self.namespacesInUse.items():
+            if key and value == uri:
+                localname = '{prefix}:{localname}'.format(
+                    prefix=key.lower(), localname=localname
+                )
+                break
 
-        for (namespace, attrlocalname), attrvalue in attrs.items():
-            lowernamespace = (namespace or '').lower()
-            prefix = self._matchnamespaces.get(lowernamespace, '')
+        for (uri, attrlocalname), attrvalue in attrs.items():
+            loweruri = (uri or '').lower()
+            prefix = self._matchnamespaces.get(loweruri)
             if prefix:
-                attrlocalname = prefix + ':' + attrlocalname
-            attrsD[str(attrlocalname).lower()] = attrvalue
-        for qname in attrs.getQNames():
-            attrsD[str(qname).lower()] = attrs.getValueByQName(qname)
-        localname = str(localname).lower()
+                attrlocalname = '{prefix}:{localname}'.format(
+                    prefix=prefix, localname=attrlocalname
+                )
+            attrsD[attrlocalname.lower()] = attrvalue
+
         self.unknown_starttag(localname, list(attrsD.items()))
+
+    def endElementNS(self, name, qname):
+        uri, localname = name
+        localname = localname.lower()
+        loweruri = str(uri or '').lower()
+        prefix = self._matchnamespaces.get(loweruri)
+
+        for key, value in self.namespacesInUse.items():
+            if key and value == uri:
+                localname = '{prefix}:{localname}'.format(
+                    prefix=key.lower(), localname=localname
+                )
+                break
+
+        self.unknown_endtag(localname)
 
     def characters(self, text):
         self.handle_data(text)
-
-    def endElementNS(self, name, qname):
-        namespace, localname = name
-        lowernamespace = str(namespace or '').lower()
-        if qname and qname.find(':') > 0:
-            givenprefix = qname.split(':')[0]
-        else:
-            givenprefix = ''
-        prefix = self._matchnamespaces.get(lowernamespace, givenprefix)
-        if prefix:
-            localname = prefix + ':' + localname
-        elif namespace and not qname: #Expat
-            for name,value in self.namespacesInUse.items():
-                if name and value == namespace:
-                    localname = name + ':' + localname
-                    break
-        localname = str(localname).lower()
-        self.unknown_endtag(localname)
 
     def error(self, exc):
         self.bozo = 1
