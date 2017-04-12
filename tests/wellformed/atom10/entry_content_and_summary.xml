kurtmckee/feedparser

commit b06df763a0465ffd9acf7b3aae7ba3104234e6d3

Applied patch to fix issue 82
git-svn-id: http://feedparser.googlecode.com/svn/trunk@344 73d2b349-402e-0410-baf4-070fd12ab5b7

feedparser/feedparser.py
@@ -230,7 +230,7 @@ class FeedParserDict(UserDict):
               'guid': 'id',
               'date': 'updated',
               'date_parsed': 'updated_parsed',
-              'description': ['subtitle', 'summary'],
+              'description': ['summary', 'subtitle'],
               'url': ['href'],
               'modified': 'updated',
               'modified_parsed': 'updated_parsed',
@@ -1556,10 +1556,10 @@ class _FeedParserMixin:
     _start_fullitem = _start_content_encoded
 
     def _end_content(self):
-        copyToDescription = self.mapContentType(self.contentparams.get('type')) in (['text/plain'] + self.html_types)
+        copyToSummary = self.mapContentType(self.contentparams.get('type')) in (['text/plain'] + self.html_types)
         value = self.popContent('content')
-        if copyToDescription:
-            self._save('description', value)
+        if copyToSummary:
+            self._save('summary', value)
 
     _end_body = _end_content
     _end_xhtml_body = _end_content

commit 95ae1d68d90bd01359a47afab23203a421e851a9

Test case for issue 59
If <summary> follows <content>, it should not be overwritten. (This is
apparently a regression introduced way back in b06df763a0465ffd9acf7b)

tests/wellformed/atom10/entry_content_and_summary.xml
@@ -1,0 +1,10 @@
+<!--
+Description: entry summary follows content with different value
+Expect:      not bozo and entries[0]['summary'] == 'Summary'
+-->
+<feed xmlns="http://www.w3.org/2005/Atom">
+<entry>
+  <content>Example Atom</content>
+  <summary>Summary</summary>
+</entry>
+</feed>