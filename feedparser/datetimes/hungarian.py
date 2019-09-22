35191a93ee7ff57254e0d0764f7064fd0866faf7
Kurt McKee <contactme@kurtmckee.org>
2019-09-22 18:53:22 -0500

    Clean up datetimes/*
    * Add headers
    * Fix whitespace
    * Remove backslash line continuations
    * Replace camelCase names

--- feedparser/datetimes/hungarian.py
+++ feedparser/datetimes/hungarian.py
@@ -1,12 +1,39 @@
-from __future__ import absolute_import, unicode_literals
+# Copyright 2010-2019 Kurt McKee <contactme@kurtmckee.org>
+# Copyright 2002-2008 Mark Pilgrim
+# All rights reserved.
+#
+# This file is a part of feedparser.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
 import re
 
 from .w3dtf import _parse_date_w3dtf
 
 # Unicode strings for Hungarian date strings
-_hungarian_months = \
-  { \
+_hungarian_months = {
     'janu\u00e1r':   '01',  # e1 in iso-8859-2
     'febru\u00e1ri': '02',  # e1 in iso-8859-2
     'm\u00e1rcius':  '03',  # e1 in iso-8859-2
@@ -19,14 +46,14 @@ _hungarian_months = \
     'okt\u00f3ber':  '10',  # f3 in iso-8859-2
     'november':      '11',
     'december':      '12',
-  }
+}
 
-_hungarian_date_format_re = \
-  re.compile(r'(\d{4})-([^-]+)-(\d{,2})T(\d{,2}):(\d{2})((\+|-)(\d{,2}:\d{2}))')
+_hungarian_date_format_re = re.compile(r'(\d{4})-([^-]+)-(\d{,2})T(\d{,2}):(\d{2})((\+|-)(\d{,2}:\d{2}))')
 
-def _parse_date_hungarian(dateString):
-    '''Parse a string according to a Hungarian 8-bit date format.'''
-    m = _hungarian_date_format_re.match(dateString)
+
+def _parse_date_hungarian(date_string):
+    """Parse a string according to a Hungarian 8-bit date format."""
+    m = _hungarian_date_format_re.match(date_string)
     if not m or m.group(2) not in _hungarian_months:
         return None
     month = _hungarian_months[m.group(2)]
@@ -37,7 +64,12 @@ def _parse_date_hungarian(dateString):
     if len(hour) == 1:
         hour = '0' + hour
     w3dtfdate = '%(year)s-%(month)s-%(day)sT%(hour)s:%(minute)s%(zonediff)s' % \
-                {'year': m.group(1), 'month': month, 'day': day,\
-                 'hour': hour, 'minute': m.group(5),\
-                 'zonediff': m.group(6)}
+                {
+                    'year': m.group(1),
+                    'month': month,
+                    'day': day,
+                    'hour': hour,
+                    'minute': m.group(5),
+                    'zonediff': m.group(6),
+                }
     return _parse_date_w3dtf(w3dtfdate)
