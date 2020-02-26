8bbb66d705e7053cb01a2a8d32f40d6eda37c649
Guts <Guts@users.noreply.github.com>
2020-02-26 08:13:36 +0100

    Applying balck formatting

--- setup.py
+++ setup.py
@@ -32,37 +32,40 @@ import sys
 sys.path.append(str(pathlib.Path(__file__).parent))
 import feedparser
 
-with open('README.rst', 'r') as f:
+with open("README.rst", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='feedparser',
+    name="feedparser",
     version=feedparser.__version__,
-    license='BSD-2-Clause',
-    description='Universal feed parser, handles RSS 0.9x, RSS 1.0, RSS 2.0, CDF, Atom 0.3, and Atom 1.0 feeds',
+    license="BSD-2-Clause",
+    description="Universal feed parser, handles RSS 0.9x, RSS 1.0, RSS 2.0, CDF, Atom 0.3, and Atom 1.0 feeds",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    author='Kurt McKee',
-    author_email='contactme@kurtmckee.org',
-    url='https://github.com/kurtmckee/feedparser',
-    download_url='https://pypi.python.org/pypi/feedparser',
-    platforms=['POSIX', 'Windows'],
-    packages=['feedparser', 'feedparser.datetimes', 'feedparser.namespaces', 'feedparser.parsers'],
-    install_requires=[
-        'sgmllib3k;python_version>="3.0"',
+    long_description_content_type="text/x-rst",
+    author="Kurt McKee",
+    author_email="contactme@kurtmckee.org",
+    url="https://github.com/kurtmckee/feedparser",
+    download_url="https://pypi.python.org/pypi/feedparser",
+    platforms=["POSIX", "Windows"],
+    packages=[
+        "feedparser",
+        "feedparser.datetimes",
+        "feedparser.namespaces",
+        "feedparser.parsers",
     ],
-    keywords=['atom', 'cdf', 'feed', 'parser', 'rdf', 'rss'],
+    install_requires=['sgmllib3k;python_version>="3.0"',],
+    keywords=["atom", "cdf", "feed", "parser", "rdf", "rss"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Markup :: XML',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup :: XML",
     ],
 )
