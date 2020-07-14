9298d638d3cc8f7249d83277c4130a8859576e9c
wei wang <wei.wang@castbox.fm>
2020-07-14 10:23:41 +0800

    support 'Yes' for itunes:block

--- feedparser/namespaces/itunes.py
+++ feedparser/namespaces/itunes.py
@@ -102,7 +102,7 @@ class Namespace(object):
 
     def _end_itunes_block(self):
         value = self.pop('itunes_block', 0)
-        self._get_context()['itunes_block'] = (value == 'yes') and 1 or 0
+        self._get_context()['itunes_block'] = (value == 'yes' or value == 'Yes') and 1 or 0
 
     def _end_itunes_explicit(self):
         value = self.pop('itunes_explicit', 0)
