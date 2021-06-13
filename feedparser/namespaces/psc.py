63bc8cf9880baf18709443f2887a2e8b84586f29
Kurt McKee <contactme@kurtmckee.org>
2021-06-12 22:03:05 -0500

    Bump copyright and version

--- feedparser/namespaces/psc.py
+++ feedparser/namespaces/psc.py
@@ -1,5 +1,5 @@
 # Support for the Podlove Simple Chapters format
-# Copyright 2010-2020 Kurt McKee <contactme@kurtmckee.org>
+# Copyright 2010-2021 Kurt McKee <contactme@kurtmckee.org>
 # Copyright 2002-2008 Mark Pilgrim
 # All rights reserved.
 #
