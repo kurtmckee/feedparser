5f5d2fa1f0788575e89c04ba7eed9038a910fc1a
Kurt McKee <contactme@kurtmckee.org>
2023-02-19 07:45:39 -0600

    Update the Sphinx configuration
    This primarily modifies the config to always enable nitpicky mode,
    to read the project version from `pyproject.toml`,
    and to rename `master_doc` to `root_doc`.

--- docs/conf.py
+++ docs/conf.py
@@ -4,24 +4,25 @@ import re
 import sys
 
 
-content = (pathlib.Path(__file__).parent.parent / 'feedparser/__init__.py').read_text()
-match = re.search(r"""__version__ = ['"](?P<version>.+?)['"]""", content)
+content = (pathlib.Path(__file__).parent.parent / 'pyproject.toml').read_text()
+match = re.search(r'''version *= *"(?P<version>.+?)"''', content)
 version = match.group('version')
 release = version
 
-# project information
+# Project information
 project = 'feedparser'
 copyright = '2010-2022 Kurt McKee, 2004-2008 Mark Pilgrim'
 language = 'en'
 
-# documentation options
-master_doc = 'index'
+# General configuration
+root_doc = 'index'
 exclude_patterns = ['_build']
+nitpicky = True
 
-# use a custom extension to make Sphinx add a <link> to feedparser.css
+# HTML options
+# Files in `html_static_path` will be copied to `_static/` when compiled.
+html_static_path = ['_static']
+
+# Use a custom extension to make Sphinx add a <link> to `feedparser.css`.
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 extensions = ['add_custom_css']
-
-# customize the html
-# files in html_static_path will be copied into _static/ when compiled
-html_static_path = ['_static']
