2893f8fa49473177eb60be423e061d705a0b6323
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2023-08-08 02:32:38 -0700

    Fix incorrect passing of flags to re.sub

--- feedparser/html.py
+++ feedparser/html.py
@@ -152,7 +152,7 @@ class BaseHTMLProcessor(sgmllib.SGMLParser):
         :rtype: None
         """
 
-        data = re.sub(r"<!((?!DOCTYPE|--|\[))", r"&lt;!\1", data, re.IGNORECASE)
+        data = re.sub(r"<!((?!DOCTYPE|--|\[))", r"&lt;!\1", data, flags=re.IGNORECASE)
         data = re.sub(r"<([^<>\s]+?)\s*/>", self._shorttag_replace, data)
         data = data.replace("&#39;", "'")
         data = data.replace("&#34;", '"')
